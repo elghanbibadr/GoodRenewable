8f0cdb1063409277cd5b91b78102df30ae66bc81
badr ghanbi <bghanbi50@gmail.com>
2023-03-20 20:02:57 +0000

    create the mobile version of navbar

---
+++ src/componenet/Nav/BookCallBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import arrow from "../../assets/arrow.svg"
+
+const BookCallBtn = () => {
+  return (
+    <button className='flex  border-2 border-paleBlack rounded-2xl px-6 py-2 items-center'>
+    <p className='mr-4'>Book a Call</p>
+    <img className='w-[20px]' src={arrow} alt='arrow right logo' />
+</button>  )
+}
+
+export default BookCallBtn
