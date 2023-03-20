752ace0a53636f124e462916da7f368bda54419f
badr ghanbi <bghanbi50@gmail.com>
2023-03-20 20:29:59 +0000

    more work on the navbar

--- src/componenet/Nav/BookCallBtn.jsx
+++ src/componenet/Nav/BookCallBtn.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import arrow from "../../assets/arrow.svg"
 
-const BookCallBtn = () => {
+const BookCallBtn = (props) => {
   return (
-    <button className='flex  border-2 border-paleBlack rounded-2xl px-6 py-2 items-center'>
+    <button className={`${props.className} flex  border-2 border-paleBlack rounded-2xl px-6 py-2 items-center`}>
     <p className='mr-4'>Book a Call</p>
     <img className='w-[20px]' src={arrow} alt='arrow right logo' />
 </button>  )
--- src/componenet/Nav/Nav.jsx
+++ src/componenet/Nav/Nav.jsx
@@ -12,19 +12,19 @@ const Nav = () => {
     const hanldeCloseIconClicked=()=>setMenuCollapse(false)
   return (
     <header>
-    <nav className='p-4 flex items-center justify-between flex-wrap'>
-     {!menuCollapse &&   <img src={logo} alt='logo' />}  
-      {menuCollapse &&  <img className='w-64 h-14'  src={GoodRenewable} alt="goodRenewable logo"/>}  
-    { !menuCollapse &&   <BookCallBtn /> }  
-  { !menuCollapse &&  <img onClick={hanldeMenuHamburgerClicked} src={menuBurger} alt="menu burger icon" />}    
-     { menuCollapse && <img onClick={hanldeCloseIconClicked} className='' src={closeIcon} alt="close icon" />}  
-     {menuCollapse &&  <ul className=' mx-auto flex flex-col items-center mt-20  w-full relative    text-black navbar__list font-bold  text-[3.2rem]'>
+    <nav className='p-4 flex items-center justify-between flex-wrap md:flex-nowrap'>
+     <img className={`${!menuCollapse ? "block":"hidden"} md:hidden`} src={logo} alt='logo' /> 
+   <img className={`${menuCollapse ? "block":"hidden"} w-64 h-14 md:block`}  src={GoodRenewable} alt="goodRenewable logo"/>
+    { !menuCollapse &&   <BookCallBtn className="md:hidden" /> }  
+  { !menuCollapse &&  <img onClick={hanldeMenuHamburgerClicked} className="md:hidden" src={menuBurger} alt="menu burger icon" />}    
+     { menuCollapse && <img onClick={hanldeCloseIconClicked} className='md:hidden' src={closeIcon} alt="close icon" />}  
+       <ul className={ ` ${!menuCollapse ? "opacity-0 invisible":"opacity-100 visible"} mx-auto md:mx-1 flex flex-col items-center mt-20 md:mt-auto  w-full md:w-auto    text-black navbar__list font-bold md:font-[400]  text-[3.2rem] md:flex-row  md:text-[1.6rem]`}>
         <li  ><a href='#'>Our Process</a> </li>
         <li><a href='#'>Get Involved</a> </li>
         <li><a href='#'>FAQ</a> </li>
         <li><a href='#'>Our Team</a> </li>
-        <li className='relative top-10'><a href='#'><BookCallBtn /></a> </li>
-     </ul>}
+        <li className='relative top-10 md:top-0'><a href='#'><BookCallBtn /></a> </li>
+     </ul>
       </nav>
     </header>
   )
