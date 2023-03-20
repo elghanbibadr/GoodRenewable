1d8fca3969e9ee55dfb8b532f047d0b84ce5f20d
badr ghanbi <bghanbi50@gmail.com>
2023-03-20 12:51:02 +0000

    customize media query

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,28 @@ module.exports = {
   ],
     theme: {
     extend: {},
+    colors: {
+     "black": "#152223e6",
+     "green": "#5ada2e",
+     "paleBlack": "#5A6364",
+     "paleWhite": "#F4FFE7",
+    },
+    screens: {
+      'sm': '360px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1440px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
