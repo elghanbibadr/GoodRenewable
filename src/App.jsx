d4751c508ff11ab2dffd37cfb4f2cfaed142179b
badr ghanbi <bghanbi50@gmail.com>
2023-03-21 14:42:40 +0000

    switch componenet name between features and desc

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Nav from './componenet/Nav/Nav'
 import Hero from './componenet/Hero/Hero'
+import Desc from './componenet/Desc'
 import Features from './componenet/Features/Features'
+
 const App = () => {
   return (
    <div className='lg:px-6 py-4'>
    <Nav />
    <Hero />
+   <Desc/>
    <Features />
    </div>
   )
---
+++ src/componenet/UI/Gridx2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Gridx2 = (props) => {
+  return (
+    <div className=' md:grid md:grid-cols-2'>{props.children}</div>
+  )
+}
