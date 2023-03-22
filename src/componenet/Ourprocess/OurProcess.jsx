f4d562bb24ad2cce0b6e5d5e8e8616cfd75d26a9
badr ghanbi <bghanbi50@gmail.com>
2023-03-22 15:18:56 +0000

    created the our process section

--- src/componenet/Ourprocess/OurProcess.jsx
+++ src/componenet/Ourprocess/OurProcess.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { ourProcessData } from './Constant'
+import OurProcessItem from './OurProcessItem'
 
 const OurProcess = () => {
   return (
-    <div className='bg-white pt-[10rem] rounded-[2.4rem] pl-6 mt-[10rem]'>
-    <h2 className='mb-10'>OurProcess</h2>
-    <p>The three-step strategy is carefully designed to maximise the likelihood of success</p>
+    <div className='bg-white pt-[10rem] rounded-[2.4rem] pl-6 mt-[10rem] '>
+    <div className='md:text-center'>
+        <h2 className='mb-10'>OurProcess</h2>
+        <p>The three-step strategy is carefully designed to maximise the likelihood of success</p>
+    </div>
+    {ourProcessData.map(({id,title,img,text})=>{
+        return <OurProcessItem id={id} title={title} img={img} text={text} />
+    })}
   </div>
   )
 }
--- src/componenet/Ourprocess/OurProcessItem.jsx
+++ src/componenet/Ourprocess/OurProcessItem.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Gridx2 } from '../UI/Gridx2'
 
-const OurProcessItem = () => {
+const OurProcessItem = (props) => {
   return (
     <Gridx2>
-        <img src={props.img} />
-        <div>
-            <div>
-                <span>{props.id}</span>
-                <h3>{props.title}</h3>
+            <div className={`${props.id==2 ? "col-start-2 row-start-2 " :""} lg:pl-14`}>
+                <div className='lg:flex lg:items-center '>
+                    <div className='bg-green self-auto w-[40px] mt-32 lg:relative lg:right-20  md:mt-0 flex font-bold text-xl text-black justify-center items-center h-[40px] p-2 rounded-full '>
+                        <span>{props.id}</span>
+                    </div>
+                    <h3 className='my-8 lg:relative lg:right-12 '>{props.title}</h3>
+                </div>
                 <p>{props.text}</p>
             </div>
-        </div>
+        <img className={`${props.id==2 ? "col-start-1 row-start-2  " :""}  w-[70%]  mx-auto mt-12 mb-12 lg:w-[60%]`} src={props.img} />
     </Gridx2>
   )
 }
