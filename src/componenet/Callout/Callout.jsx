b166546ecbad61854b04f95d8586f1a0d092c966
badr ghanbi <bghanbi50@gmail.com>
2023-03-22 20:30:06 +0000

    switch places between gridx3 and the button

--- src/componenet/Callout/Callout.jsx
+++ src/componenet/Callout/Callout.jsx
@@ -5,14 +5,14 @@ import { CalloutCardsData  } from './CalloutCardsData'
 import { Gridx3 } from '../UI/Gridx3'
 const Callout = () => {
   return (
-    <div className='mt-40 p-4'>
+    <div className='mt-40 p-4 md:grid md:grid-rows-[auto] items-center'>
         <div className='md:text-center'>
             <h3>Get <strong>involved early</strong></h3>
             <p className='my-10 lg:w-1/2 lg:mx-auto'>
             GoodRenewable is in the process of running workshops and consultations across the UK. We want to hear from communities, developers and landowners interested in being part of our pioneer projects. Book your discovery call below.
             </p>
         </div>
-        <BookCallBtn fill='white' className='py-4 md:mx-auto mb-20 bg-black text-white' text='Book a Discovery Call'/>
+        <BookCallBtn fill='white' className='py-4 md:row-start-3 md:mx-auto mb-20 bg-black text-white' text='Book a Discovery Call'/>
      <Gridx3>
        {CalloutCardsData.map(({id,title,text,img})=>{
           return <CalloutCard id={id} title={title} text={text} img={img} />;
