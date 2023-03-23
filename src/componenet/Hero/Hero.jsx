ed274dc39090f0ad82a56d1ffeeb2fc27f2b72ed
badr ghanbi <bghanbi50@gmail.com>
2023-03-23 17:10:49 +0000

    more troubleshooting on the navbar section

--- src/componenet/Hero/Hero.jsx
+++ src/componenet/Hero/Hero.jsx
@@ -8,11 +8,12 @@ const Hero = () => {
         <div className='mt-10 lg:mt-40 '>
         <h1 className=' md:w-[70%] lg:w-[80%]'>Harness the<strong className='strong-small' > Power</strong> of Community Engagement for <strong className='strong--taller'>Renewable</strong> Energy Projects</h1>
         <p className='text-black my-6 w-4/5 lg:w-1/3'>Creating clean energy projects with community support at the centre.</p>
-         <BookCallBtn text="Book a Discovery Call" className="py-2 book-discoveryCall-btn bg-black text-white hover:bg-paleWhite hover:text-black md:py-4"  />
+         <BookCallBtn text="Book a Discovery Call" className="py-3 book-discoveryCall-btn bg-black text-white hover:bg-paleWhite hover:text-black md:py-4"  />
         </div>
         <svg  className='  mx-auto absolute mt-10 md:w-1/2 md:right-4 md:top-[45%] lg:top-[60%]'  viewBox="0 0 753 483" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_0_1)">
 <path d="M690.744 371H684.363L686.244 269.802H688.465L690.744 371Z" fill="#DBDBDB"/>
+
 <path d="M684.41 371H690.744L688.652 278.146C688.711 300.17 688.293 343.114 684.41 371Z" fill="#828282"/>
 <path d="M687.216 271.444C690.2 271.444 692.619 269.029 692.619 266.051C692.619 263.072 690.2 260.657 687.216 260.657C684.232 260.657 681.812 263.072 681.812 266.051C681.812 269.029 684.232 271.444 687.216 271.444Z" fill="#DBDBDB"/>
 <path d="M683.658 270.102C684.588 270.922 685.773 271.396 687.012 271.446C688.252 271.496 689.471 271.118 690.464 270.375C691.457 269.633 692.163 268.572 692.464 267.37C692.765 266.169 692.642 264.901 692.115 263.779C691.259 265.454 689.285 268.514 686.004 269.388C685.117 269.622 684.339 269.864 683.658 270.102Z" fill="#828282"/>
--- src/componenet/Nav/Nav.jsx
+++ src/componenet/Nav/Nav.jsx
@@ -16,14 +16,14 @@ const Nav = () => {
     <nav className=' flex pt-4  items-center justify-between flex-wrap relative md:flex-nowrap md:pt-6'>
      <img className={`${!menuCollapse ? "block":"hidden"} md:hidden`} src={logo} alt='logo' /> 
    <img className={`${menuCollapse ? "block":"hidden"} w-64 h-14 md:block lg:h-[4.2rem] lg:w-[32rem] lg:relative lg:right-16`}  src={GoodRenewable} alt="goodRenewable logo"/>
-    { !menuCollapse &&   <BookCallBtn text="Book a Call" className="py-2 book-call-btn md:hidden" fill="#152223" /> }  
+    { !menuCollapse &&   <BookCallBtn text="Book a Call" className="py-2 book-call-btn  hover:bg-black hover:text-white  md:hidden" fill="#152223" /> }  
   { !menuCollapse &&  <img onClick={hanldeMenuHamburgerClicked} className="md:hidden" src={menuBurger} alt="menu burger icon" />}    
-     { menuCollapse && <img onClick={hanldeCloseIconClicked} className='md:hidden' src={closeIcon} alt="close icon" />}  
-       <ul className={ ` ${!menuCollapse ? "opacity- invisible h-0":"opacity-100 h-screen  visible"} mx-auto md:mx-1 md:visible md:h-fit  md:opacity-100 flex flex-col items-center mt-20 md:mt-0  w-full md:w-auto    text-black navbar__list font-bold md:font-[400]  text-[3.2rem] md:flex-row  md:text-[1.6rem]`}>
+     { menuCollapse && <img onClick={hanldeCloseIconClicked} className='md:hidden px-3' src={closeIcon} alt="close icon" />}  
+       <ul className={ ` ${!menuCollapse ? "opacity-0 invisible h-0":"opacity-100 h-screen  visible"} mx-auto transition-opacity duration-1000 md:mx-1 md:visible md:h-fit  md:opacity-100 flex flex-col items-center mt-20 md:mt-0  w-full md:w-auto    text-black navbar__list font-bold md:font-[400]  text-[3.2rem] md:flex-row  md:text-[1.6rem]`}>
           {listLinks.map((link,index) =>{
                 return <li  key={index}><a href='#'>{link}</a></li>
             })}
-        <li className='relative top-10 md:top-0 '><a href='#'><BookCallBtn className='book-call-btn' text="Book a Call" fill="#152223" /></a> </li>
+        <li className='relative top-10 md:top-0 '><a href='#'><BookCallBtn className='book-call-btn  hover:bg-black hover:text-white' text="Book a Call" fill="#152223" /></a> </li>
      </ul>
       </nav>
     </header>
