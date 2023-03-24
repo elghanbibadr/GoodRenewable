224ec82a904c266aa6898fd4e715494d4c4ea4d5
badr ghanbi <bghanbi50@gmail.com>
2023-03-24 17:48:39 +0000

    add the team linkdin link

--- src/componenet/OurTeam/TeamData.js
+++ src/componenet/OurTeam/TeamData.js
@@ -7,18 +7,21 @@ export const TeamData=[
         id:1,
         name:"Alexi Makris",
         role:"Alexi is a renewable energy  consultant with a background in education, training, and leading community workshops.",
-        img:alexiImage
+        img:alexiImage,
+        href:"https://www.linkedin.com/in/aleximakris/"
     },
     {
         id:2,
         name:"Janez Tratnik",
         role:"Janez has more than 10 years experience developing renewable energy projects across Europe and the UK",
-        img:janezImage
+        img:janezImage,
+        href:"https://www.linkedin.com/in/janez-tratnik-43927113/"
     },
     {
         id:3,
         name:"David Carden",
         role:"David is a digital expert and product manager with experience building platforms for the UK power sector",
-        img:davidImage
+        img:davidImage,
+        href:"https://www.linkedin.com/in/davidacarden/"
     },
 ]
