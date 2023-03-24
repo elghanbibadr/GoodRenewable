41772d73a960a0194ad1694c79e1202ed1551e67
badr ghanbi <bghanbi50@gmail.com>
2023-03-24 21:00:11 +0000

    fix the issue on the step number container stretching

--- src/componenet/Ourprocess/OurProcess.jsx
+++ src/componenet/Ourprocess/OurProcess.jsx
@@ -7,7 +7,7 @@ const OurProcess = () => {
     <div className='bg-white pt-[10rem] rounded-[2.4rem] pl-6 mt-[10rem] '>
     <div className='md:text-center'>
         <h2 className='mb-10'>OurProcess</h2>
-        <p>The three-step strategy is carefully designed to maximise the likelihood of success</p>
+        <p className='md:mb-20'>The three-step strategy is carefully designed to maximise the likelihood of success</p>
     </div>
     {ourProcessData.map(({id,title,img,text})=>{
         return <OurProcessItem id={id} title={title} img={img} text={text} />
--- src/componenet/Ourprocess/OurProcessItem.jsx
+++ src/componenet/Ourprocess/OurProcessItem.jsx
@@ -5,8 +5,8 @@ const OurProcessItem = (props) => {
   return (
     <Gridx2>
             <div className={`${props.id==2 ? "col-start-2 row-start-2 " :""} lg:pl-14`}>
-                <div className='lg:flex lg:items-center '>
-                    <div className='bg-green self-auto w-[40px] mt-32 lg:relative lg:right-20  md:mt-0 flex font-bold text-xl text-black justify-center items-center h-[40px] p-2 rounded-full '>
+                <div className='lg:grid lg:grid-cols-[40px__1fr]  '>
+                    <div className='bg-green self-center  w-[40px] mt-32 lg:relative lg:right-20  md:mt-0 flex font-bold text-xl text-black justify-center items-center h-[40px] p-2 rounded-full '>
                         <span>{props.id}</span>
                     </div>
                     <h3 className='my-8 lg:relative lg:right-12 '>{props.title}</h3>
