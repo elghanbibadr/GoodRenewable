e371cec5b2877efcad476c0b91b81b9845f3ae6c
badr ghanbi <bghanbi50@gmail.com>
2023-03-25 13:20:13 +0000

    add animation to header and feature section

--- src/componenet/Features/Map.jsx
+++ src/componenet/Features/Map.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import desktopMap from "../../assets/desktopMap.svg"
 import mobileMap from "../../assets/mobileMap.svg"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 export const Map = () => {
+
+  AOS.init();
+
+
   return (
-    <div className='mt-40 p-4 mb-40 map '>  
+    <div data-aos-offset="400"  data-aos="flip-up" className='my-80 p-4 mb-40 map '>  
     <img  className='hidden md:block mx-auto lg:w-[80%]' src={desktopMap} />
     <img  className='mx-auto md:hidden' src={mobileMap} />
     </div>
