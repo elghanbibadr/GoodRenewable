aebc3a8afd8d3841d15255a0dbd9fbb26b83e758
badr ghanbi <bghanbi50@gmail.com>
2023-03-26 16:06:45 +0000

    tweaked the map section

--- src/componenet/Features/Map.jsx
+++ src/componenet/Features/Map.jsx
@@ -4,7 +4,7 @@ import mobileMap from "../../assets/mobileMap.svg"
 export const Map = () => {
   return (
     <div   data-aos="flip-up" className='my-40 p-4 mb-40 map '  data-aos-offset="300">  
-    <img  className='hidden md:block mx-auto lg:w-[80%]' src={desktopMap} alt='map image for desktops' loading='lazy' />
+    <img  className='hidden md:block  ' src={desktopMap} alt='map image for desktops' loading='lazy' />
     <img  className='mx-auto md:hidden' src={mobileMap}  alt='map image for mobile' loading='lazy' />
     </div>
   )
