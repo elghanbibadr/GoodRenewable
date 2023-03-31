6987acddc46295ac5f174b7cff0f5830f262816b
badr ghanbi <bghanbi50@gmail.com>
2023-03-31 14:15:58 +0000

    more fix

--- src/componenet/Features/Map.jsx
+++ src/componenet/Features/Map.jsx
@@ -3,10 +3,9 @@ import mobileMap from "../../assets/mobileMap.svg"
 
 export const Map = () => {
   return (
-    // div  data-aos="flip-up"  className='my-60 max-w-[88%] mx-auto map '
-    <div data-aos="zoom-in" div className="my-60  md:max-w-[96%] mx-auto  map" data-aos-offset="300" >  
-    <img   className=' max-w-[100%]   hidden object-fit  md:block  ' src={desktopMap} alt='map image for desktops' loading='lazy' />
-    <img  className=' max-w-[100%]  object-fit md:hidden' src={mobileMap}  alt='map image for mobile' loading='lazy' />
+    <div data-aos="zoom-in" div className="my-60  md:max-w-[96%]   map" data-aos-offset="300" >  
+    <img   className=' max-w-[100%] mx-auto  hidden object-fit  md:block  ' src={desktopMap} alt='map image for desktops' loading='lazy' />
+    <img  className=' max-w-[100%] mx-auto  object-fit md:hidden' src={mobileMap}  alt='map image for mobile' loading='lazy' />
     </div>
   )
 }
--- src/componenet/UI/Layout.jsx
+++ src/componenet/UI/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout = (props) => {
   return (
-    <div className=' max-w-[1440px] lg:mx-auto px-6   lg:px-10  '>
+    <div className=' max-w-[1440px] lg:mx-auto px-4 md:px-6   lg:px-10  '>
         {props.children}
     </div>
   )
